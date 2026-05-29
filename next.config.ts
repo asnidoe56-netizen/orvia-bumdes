80e289ad5de175eff81e476cc372205a78697b1b
Afkar Mohamad <akmalnurdiansyahmohamad@gmail.com>
2026-05-30 03:52:06 +0800

    Allow production domains for server actions

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
-import type { NextConfig } from "next";
+﻿import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    serverActions: {
+      allowedOrigins: [
+        "www.inovasigorut.online",
+        "inovasigorut.online",
+        "orvia-bumdes.vercel.app",
+        "*.vercel.app",
+      ],
+    },
+  },
 };
 
 export default nextConfig;
